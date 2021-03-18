5b793c2fb04cfbb7aa5132e3517c0ffe6c16548f
jbytecode <mhsatman@gmail.com>
2021-03-18 13:07:51 +0300

    add tests

--- test/lmsreg/core_test.clj
+++ test/lmsreg/core_test.clj
@@ -1,7 +1,31 @@
 (ns lmsreg.core-test
   (:require [clojure.test :refer :all]
-            [lmsreg.core :refer :all]))
+            [lmsreg.core :refer :all]
+            [lmsreg.ols :refer :all]
+            [lmsreg.lms :refer :all]
+            [clojure.core.matrix :refer  :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest add-one-to-x-test
+  (testing "Adding ones to design matrix"
+    (is [[1 1] [1 2] [1 3] [1 4] [1 5]]
+        (add-ones-to-x [1 2 3 4 5]))))
+
+(deftest ols-test
+  (testing "Ordinary least squares - fit only"
+    (is [0.0 2.0]
+        (let
+         [x1 [1 2 3 4 5 6 7 8 9 10]
+          x (add-ones-to-x x1)
+          y [2 4 6 8 10 12 14 16 18 20]
+          betas (ols-fit x y)]
+          betas))))
+
+(deftest lms-random-test
+  (testing "Least Median of Squares - Random algorithm with 500 subsets"
+    (is [0.0 2.0]
+        (let
+         [x1 [1 2 3 4 5 6 7 8 9 100]
+          x (add-ones-to-x x1)
+          y [2 4 6 8 10 12 14 16 18 20]
+          betas (lms-random x y 500)]
+          betas))))
