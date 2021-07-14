c91355b93e80b3da60b45b1198138643cae43915
jbytecode <mhsatman@gmail.com>
2021-07-14 14:18:36 +0300

    c-step for lts

--- src/lmsreg/lts.clj
+++ src/lmsreg/lts.clj
@@ -11,7 +11,7 @@
     "
   [x y betas]
   (let
-   [n         (count x)
+   [n         (count y)
     p         (if (= (matrix/dimensionality x) 1) 1 (count (first x)))
     h         (/ (+ n p 1) 2)
     residuals (matrix/sub y (matrix/mmul x betas))
@@ -21,6 +21,21 @@
                    (take h)
                    (apply +))] sumsqres))
 
+
+(defn c-step [x y indices]
+  (let
+   [n             (count y)
+    p             (if (= (matrix/dimensionality x) 1) 1 (count (first x)))
+    h             (/ (+ n p 1) 2)
+    xsub          (vec (map #(nth x %1) indices))
+    ysub          (vec (map #(nth y %1) indices))
+    betas         (ols/ols-fit xsub ysub)
+    residuals     (matrix/sub y (matrix/mmul x betas))
+    sqres         (map-indexed #(vec [%1 (Math/pow %2 2.0)]) residuals)
+    ordered-couples  (sort-by second sqres)
+    best-indices (->> ordered-couples
+                      (take h)
+                      (map first))] best-indices))
 
 
 
--- test/lmsreg/lts_test.clj
+++ test/lmsreg/lts_test.clj
@@ -10,3 +10,23 @@
             (ols/add-ones-to-x (:year datasets/phones-dataset))
             (:calls datasets/phones-dataset)
             [-56 1.16])  6.51679999999998))))
+
+(deftest lts-single-c-step
+  (testing "Single Concentration step for LTS"
+    (is (=
+         (-> (lts/c-step
+              [[1 1] [1 2] [1 3] [1 4] [1 5] [1 6]]
+              [2 4 6 8 11 120]
+              [3 4])
+             (sort))
+
+         (list 0 1 2 3 4)))
+
+    (is (=
+         (-> (lts/c-step
+              [[1 1] [1 2] [1 3] [1 4] [1 5] [1 6]]
+              [2 4 6 8 11 120]
+              [2 1])
+             (sort))
+
+         (list 0 1 2 3 4)))))
